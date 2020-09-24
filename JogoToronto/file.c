2ee08b5937d16616726bf41bcb8b9c48bd8de658
Lucas Henrique <lcarvalho.henrique@gmail.com>
2020-09-24 20:06:17 -0300

    edição teste

--- JogoToronto/file.c
+++ JogoToronto/file.c
@@ -15,7 +15,7 @@ int main()
     al_clear_to_color(al_map_rgb(255, 255, 255));
     al_draw_bitmap(imagem, 900, 400,0);
     char tcaixa[50] = "Meu título";
-    char titulo[100] = "Sou uma caixa de texto";
+    char titulo[100] = "Sou uma caixa de texto editada";
     char texto[200] = "A mensagem a ser exibida deve ficar aqui";
     int r = al_show_native_message_box(NULL, tcaixa, titulo, texto, NULL, ALLEGRO_MESSAGEBOX_OK_CANCEL);
     printf("%i", r);
